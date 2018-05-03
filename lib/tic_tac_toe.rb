dae6713cbf2f730fe460715d9b698b4bf0ad714f
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-05-03 19:23:16 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,7 +41,7 @@ class TicTacToe
     index.between?(0,8) && !position_taken?(index)
   end
   def current_player
-    player = turn_count(@board).even? ?  "X" : "O"
+    player = turn_count(@board).even? ? "X" : "O"
   end
   
   def turn_count
